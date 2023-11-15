99b4f0844b4f663be3533f4d7f3424893ca2fd30
aldisti <alessandrodiste99@gmail.com>
2023-11-15 14:45:25 +0100

    fixed some bugs in clean and fclean

--- srcs/tools/clean.sh
+++ srcs/tools/clean.sh
@@ -2,29 +2,19 @@
 
 DOCKER_PATH="./srcs/docker-compose.yml"
 
-echo "--> doing compose down"
 docker compose -f "$DOCKER_PATH" down
 echo "--> compose down completed [$?]"
 
-containers="$(docker ps -a | \
-	grep 'mariadb\|wordpress\|nginx' | \
-	awk '{print $1}' | \
-	tr '\n' '\ ')"
+containers="$(docker ps -a | grep 'mariadb\|wordpress\|nginx' | \
+	awk '{print $1}' | tr '\n' '\ ')"
 if [ -n "$containers" ]; then
-	echo "--> removing containers"
 	docker rm $containers
 	echo "--> containers removed [$?]"
-else
-	echo "--> no containers found"
 fi
 
-volumes="$(docker volume ls -q | \
-	grep 'mariadb\|wordpress')"
+volumes="$(docker volume ls -q | grep 'mariadb\|wordpress')"
 if [ -n "$volumes" ]; then
-	echo "--> removing docker volumes"
 	docker volume rm $volumes
 	echo "--> docker volumes removed [$?]"
-else
-	echo "--> no volumes found"
 fi
 
--- srcs/tools/fclean.sh
+++ srcs/tools/fclean.sh
@@ -4,28 +4,19 @@ DOCKER_PATH="./srcs/tools/docker-compose.yml"
 
 images="$(docker images -a | grep 'mariadb\|nginx\|wordpress' | awk '{print $3}' | tr '\n' '\ ')"
 if [ -n "$images" ]; then
-	echo "--> removing images"
 	docker image rm $images
 	echo "--> images removed [$?]"
-else
-	echo "--> no images found"
 fi
 
 network="$(docker network ls | grep 'inception' | awk '{print $1}')"
 if [ -n "$network" ]; then
-	echo "--> removing network"
 	docker network rm $network
 	echo "--> network removed [$?]"
-else
-	echo "--> no network found"
 fi
 
-files="$(sudo ls "$HOME/data/mariadb $HOME/data/wordpress")"
-if [ -n "$files" ]; then
-	echo "--> removing volume files"
-	sudo rm -rf "$HOME/data/mariadb/*" "$HOME/data/wordpress/*"
+if [ -d "$HOME/data/mariadb" ] || [ -d "$HOME/data/wordpress" ]; then
+	sudo rm -rf "$HOME/data/mariadb"
+	sudo rm -rf "$HOME/data/wordpress"
 	echo "--> volume files removed [$?]"
-else
-	echo "--> no volume files"
 fi
 
